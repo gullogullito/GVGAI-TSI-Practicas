fb185d6354ac0148fb5f3ce4962924b0e4fa959a
Julio Pérez <101260255+gullogullito@users.noreply.github.com>
2024-03-25 20:10:22 +0100

    25/03 hasta las 8

--- tracks/singlePlayer/evaluacion/src_PEREZ_CABEZA_JULIO/AgenteDijkstra.java
+++ tracks/singlePlayer/evaluacion/src_PEREZ_CABEZA_JULIO/AgenteDijkstra.java
@@ -2,6 +2,7 @@ package tracks.singlePlayer.evaluacion.src_PEREZ_CABEZA_JULIO;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Stack;
 
@@ -22,7 +23,8 @@ public class AgenteDijkstra extends AbstractPlayer {
 		Vector2d fescala;
 		Vector2d portal;
 		
-		PriorityQueue<Nodo> abiertos;		//Nodos abiertos y no visitados
+		PriorityQueue<Nodo> sucesores;		//Nodos abiertos y no visitados
+		ArrayList<Nodo> visitados;
 		Stack<Types.ACTIONS> ruta ;			//Pila con la ruta, siempre sacamos la accion mas longeva
 		boolean tengoRuta;					//0 si no hay ruta planeada, 1 caso contrario
 		ArrayList<Observation>[][] mygrid;	//Grid
@@ -45,8 +47,10 @@ public class AgenteDijkstra extends AbstractPlayer {
 	        portal.y = Math.floor(portal.y / fescala.y);
 	        
 	        ruta = new Stack<>();
-	        abiertos = new PriorityQueue<>();
+	        sucesores = new PriorityQueue<>();
+	        visitados = new ArrayList<>();
 	        tengoRuta = false;
+	        mygrid = stateObs.getObservationGrid();
 		}
 		
 		/**
@@ -100,27 +104,95 @@ public class AgenteDijkstra extends AbstractPlayer {
 			
 		}
 
-		private void CosteUniforme(StateObservation stateObs) {
+		private void CosteUniforme(Nodo inicial, StateObservation stateObs) {
+			
 			Vector2d pos = new Vector2d(stateObs.getAvatarPosition().x / fescala.x,
 					stateObs.getAvatarPosition().y / fescala.y);
-			Nodo actual = new Nodo(pos, 0);
-			
-			while (actual.getPos() != portal ) {
+			Nodo actual = new Nodo(null, Types.ACTIONS.ACTION_NIL, pos, 0);
+			ArrayList<Nodo> expandidos = new ArrayList<>();
+			sucesores.add(actual);
+						
+			while (!tengoRuta) {
 				
+				actual = sucesores.poll();				
+				visitados.add(actual);
+				sucesores.remove(actual);
+				
+				if(actual.getPos() != portal) {
+				
+					expandidos = actual.expandir(portal);
+					
+					for( int i = 0; i < expandidos.size(); i++) {
+						Nodo sucesor = expandidos.get(i);
+						sucesores.add(sucesor);
+						Integer distancia;
+						
+						if (!visitados.contains(sucesor) && actual.getCoste() > actual.getCoste() +  distancia) {
+							sucesor.setCoste(actual.getCoste()+ distancia);
+						}
+						
+					}
+				}
+				
+				else
+					tengoRuta = true;
+			}
+		}
+		
+		private boolean esObstaculo(Vector2d casilla) {
+			boolean obstaculo = false;
+			
+			int i = 0;
+			
+			
+			while ( i < mygrid[(int)casilla.x][(int)casilla.y].size() && !obstaculo) {
+	            if(mygrid[(int)casilla.x][(int)casilla.y].get(i).itype == 'w' || mygrid[(int)casilla.x][(int)casilla.y].get(i).itype == 't')
+	                obstaculo = true;
+	            i++;
 			}
 		}
 }
 
 
 
-class Nodo {
-    private Vector2d pos;
-    private float coste;
+class Nodo implements Comparable<Nodo> {
+	
+	private Nodo padre;				//Padre del nodo
+	protected ArrayList<Nodo> hijos;//Lista de hijos, pueden ser 4
+	private Types.ACTIONS accion;	//Accion que hizo falta para llegar al nodo
+    private Vector2d pos;			//Posicion del nodo
+    
+    private Integer coste;			//Coste 
 
     // Constructor
-    public Nodo(Vector2d pos, float coste) {
+    public Nodo(Nodo padre, Types.ACTIONS accion, Vector2d pos, Integer coste){
+        this.padre = padre;
+        this.accion = accion;
+        hijos = new ArrayList<>(4);
         this.pos = pos;
-        this.coste = coste;
+    }
+    
+    public ArrayList<Nodo> expandir(Vector2d meta){
+    	
+    	hijos.add(new Nodo(this, ACTIONS.ACTION_UP, new Vector2d(pos.x, pos.y-1), coste + 1));	//Nodo arriba
+    	
+    	hijos.add(new Nodo(this, ACTIONS.ACTION_DOWN, new Vector2d(pos.x, pos.y+1), coste + 1));	//Nodo abajo
+    	
+    	hijos.add(new Nodo(this, ACTIONS.ACTION_LEFT, new Vector2d(pos.x-1, pos.y), coste + 1));	//Nodo izqda
+    	
+    	hijos.add(new Nodo(this, ACTIONS.ACTION_RIGHT, new Vector2d(pos.x+1, pos.y), coste + 1));	//Nodo dcha
+    	
+    	
+    	return hijos;
+    }
+    	
+        
+    public Nodo getPadre() {
+        return padre;
+    }
+    
+    public ArrayList<Nodo> getHijos() {
+        return hijos;
     }
 
     public Vector2d getPos() {
@@ -131,11 +203,33 @@ class Nodo {
     	this.pos = pos;
     }
 
-    public float getCoste() {
+    public Integer getCoste() {
         return coste;
     }
     
-    public void setCoste(float coste) {
+    public void setCoste(Integer coste) {
     	this.coste = coste;
     }
+    
+    
+    public static ACTIONS orientationToAction(Vector2d orientation) {
+ 	   if (orientation.x == 1)
+ 	    return ACTIONS.ACTION_RIGHT;
+ 	   if (orientation.x == -1)
+ 	    return ACTIONS.ACTION_LEFT;
+ 	   if (orientation.y == 1)
+ 	    return ACTIONS.ACTION_DOWN;
+ 	   if (orientation.y == -1)
+ 	    return ACTIONS.ACTION_UP;
+ 	   return ACTIONS.ACTION_NIL;
+ }
+
+
+	@Override
+	public int compareTo(Nodo o) {
+		// TODO Auto-generated method stub
+		return this.coste > o.coste ? 1 : -1;		//Orden descendiente
+	}
 }
+
+
